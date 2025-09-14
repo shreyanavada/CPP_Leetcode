466bd3bbf945c6c0cbd57ef5d251587b2ca5b79f
Shreya Navada <shreyanavada26@gmail.com>
2025-09-14 09:36:57 +0530

    Create solution.cpp
    Given a positive integer n, return the smallest positive integer that is a multiple of both 2 and n.
     
    
    Example 1:
    
    Input: n = 5
    Output: 10
    Explanation: The smallest multiple of both 5 and 2 is 10.
    Example 2:
    
    Input: n = 6
    Output: 6
    Explanation: The smallest multiple of both 6 and 2 is 6. Note that a number is a multiple of itself.
     
    
    Constraints:
    
    1 <= n <= 150

---
+++ Math/Smallest Even Multiple/solution.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int smallestEvenMultiple(int n) {
+        for(int i=1 ; i<=10 ; i++)
+        {
+            if( (n*i) % 2 == 0)
+            {
+                return (n*i) ;
+            }
+        }
+        return 0;
+    }
+};
