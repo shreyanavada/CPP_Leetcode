063ec3015ea0317fe4c31076a0733b5861f113d8
Shreya Navada <shreyanavada26@gmail.com>
2025-09-22 22:17:59 +0530

    Create solution.cpp
    You are given an array nums consisting of positive integers.
    
    Return the total frequencies of elements in nums such that those elements all have the maximum frequency.
    
    The frequency of an element is the number of occurrences of that element in the array.
    
     
    
    Example 1:
    
    Input: nums = [1,2,2,3,1,4]
    Output: 4
    Explanation: The elements 1 and 2 have a frequency of 2 which is the maximum frequency in the array.
    So the number of elements in the array with maximum frequency is 4.
    Example 2:
    
    Input: nums = [1,2,3,4,5]
    Output: 5
    Explanation: All elements of the array have a frequency of 1 which is the maximum.
    So the number of elements in the array with maximum frequency is 5.
     
    
    Constraints:
    
    1 <= nums.length <= 100
    1 <= nums[i] <= 100

---
+++ HashTable/Count Elements With Maximum Frequency/solution.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int maxFrequencyElements(vector<int>& nums) {
+        if(nums.size()==1)
+        {
+            return 1;
+        }
+    int arr[106] = { 0 }; // initialiazing the array elements to 0 .
+    int maxi = INT_MIN;
+    int ele;
+    int count=0;
+    for (int i = 0; i < nums.size();i++)
+    {
+        arr[nums[i]]++;
+        
+        if (maxi < arr[nums[i]])
+        {
+            maxi = arr[nums[i]];
+            ele = nums[i];
+        }
+    }
+    for (int i = 0;i < 106;i++)
+    {
+        if (maxi == arr[i])
+        {
+            count++;
+        }
+    }
+    return count * maxi;
+    }
+};
