638ef051080afc78824f376065711ca428a918ab
Shreya Navada <shreyanavada26@gmail.com>
2025-10-10 20:42:27 +0530

    Create solution.cpp
    Given an array nums of n integers where nums[i] is in the range [1, n], return an array of all the integers in the range [1, n] that do not appear in nums.
    
     
    
    Example 1:
    
    Input: nums = [4,3,2,7,8,2,3,1]
    Output: [5,6]
    Example 2:
    
    Input: nums = [1,1]
    Output: [2]
     
    
    Constraints:
    
    n == nums.length
    1 <= n <= 105
    1 <= nums[i] <= n
     
    
    Follow up: Could you do it without extra space and in O(n) runtime? You may assume the returned list does not count as extra space.

---
+++ HashTable/Find All Numbers Disappeared in an Array/solution.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<int> findDisappearedNumbers(vector<int>& nums) {
+        int n =nums.size();
+        vector<int>arr(n+1,0);
+        vector<int>ans;
+        for(int i=1 ;i<=n ;i++)
+        {
+            arr[nums[i-1]]++;
+        }
+        for(int i=1 ;i<=n ;i++)
+        {
+            if(arr[i]==0)
+            {
+                ans.push_back(i);
+            }
+        }
+
+        return ans;
+    }
+};
