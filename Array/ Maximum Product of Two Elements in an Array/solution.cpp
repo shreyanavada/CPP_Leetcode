c3319d8fe3290d44da233110f6cf68d38ad71215
Shreya Navada <shreyanavada26@gmail.com>
2025-11-02 22:54:47 +0530

    Implement maxProduct function to find max product
    Given the array of integers nums, you will choose two different indices i and j of that array. Return the maximum value of (nums[i]-1)*(nums[j]-1).
     
    
    Example 1:
    
    Input: nums = [3,4,5,2]
    Output: 12 
    Explanation: If you choose the indices i=1 and j=2 (indexed from 0), you will get the maximum value, that is, (nums[1]-1)*(nums[2]-1) = (4-1)*(5-1) = 3*4 = 12. 
    Example 2:
    
    Input: nums = [1,5,4,5]
    Output: 16
    Explanation: Choosing the indices i=1 and j=3 (indexed from 0), you will get the maximum value of (5-1)*(5-1) = 16.
    Example 3:
    
    Input: nums = [3,7]
    Output: 12
     
    
    Constraints:
    
    2 <= nums.length <= 500
    1 <= nums[i] <= 10^3

---
+++ Array/ Maximum Product of Two Elements in an Array/solution.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int maxProduct(vector<int>& nums) {
+        int maxi =0 ;
+        for(int i =0 ;i<nums.size() ;i++)
+        {
+            for(int j=i+1 ;j<nums.size() ;j++)
+            {
+                if( maxi < (nums[i]-1) * (nums[j]-1))
+                {
+                    maxi = (nums[i]-1) * (nums[j]-1) ;
+                }
+            }
+        }
+        return maxi;
+    }
+};
