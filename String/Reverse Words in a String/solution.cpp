569f65f20c02a393529fe955c6e093c140a8647f
Shreya Navada <shreyanavada26@gmail.com>
2026-02-08 00:44:06 +0530

    Update solution.cpp
    Given an input string s, reverse the order of the words.
    
    A word is defined as a sequence of non-space characters. The words in s will be separated by at least one space.
    
    Return a string of the words in reverse order concatenated by a single space.
    
    Note that s may contain leading or trailing spaces or multiple spaces between two words. The returned string should only have a single space separating the words. Do not include any extra spaces.
    
     
    
    Example 1:
    
    Input: s = "the sky is blue"
    Output: "blue is sky the"
    Example 2:
    
    Input: s = "  hello world  "
    Output: "world hello"
    Explanation: Your reversed string should not contain leading or trailing spaces.
    Example 3:
    
    Input: s = "a good   example"
    Output: "example good a"
    Explanation: You need to reduce multiple spaces between two words to a single space in the reversed string.
     
    
    Constraints:
    
    1 <= s.length <= 104
    s contains English letters (upper-case and lower-case), digits, and spaces ' '.
    There is at least one word in s.

--- String/Reverse Words in a String/solution.cpp
+++ String/Reverse Words in a String/solution.cpp
@@ -1,71 +1,45 @@
+// Stack is Used to crack this problem :
+// time complexity : O(n)        space complex: O(n) because of stack .
 class Solution {
 public:
     string reverseWords(string s) {
-        
-        string str;
-        string ans;
-        string neww;
-        int index; // marking the last index if there are any spaces at the last .
-        int beginn;// marking the first index if there are any spaces at the beginning .
-        int count=0 ;
-        for(int i = s.size()-1 ; i>=0 ;i--)
+        stack<string>ans;
+        string str="";
+        for(int i=0 ;i<s.size() ;i++)
         {
-            if(isalpha(s[i]))
-            {
-                index = i;
-                break;
-            }
-        }
-        for(int i = 0; i<s.size() ;i++)
-        {
-            if(isalpha(s[i]) || isdigit(s[i]))
-            {
-                beginn = i;
-                break;
-            }
-        }
-
-        for(int i =index ; i>=beginn ;i--)
-        {
-            if(i==beginn)
-            {
-                str += s[i];
-                reverse(str.begin() ,str.end());
-                ans += str ;
-            }
-            if(isalpha(s[i]) || isdigit(s[i]))
+            if(isalnum(s[i]) )
             {
                 str += s[i];
             }
-           
             else
             {
-                reverse(str.begin() ,str.end());
-                ans += str + " ";
-                str.clear();
+                if(!(str.empty()))
+                {
+                    ans.push(str);
+                    str.clear();
+                }
+                
+                
             }
         }
-        for(int i=0 ;i<ans.size() ;i++)
+        if(!(str.empty()))
         {
-            if(isalpha(ans[i])||isdigit(s[i]))
-            {
-                neww += ans[i];
-                count=0;
-            }
-            else 
-            {
-                count++;
-                if(count==1)
-                {
-                    neww += " ";
-                }
-                else
-                {
-                    continue;
-                }
-               
-            }
+            ans.push(str);
         }
-        return neww;
+        string output;
+        int sizee = ans.size();
+        
+        while(!(ans.empty()))
+        {
+            if(sizee == 1)
+            {
+                output += ans.top();
+                break;
+            }
+            output += ans.top()+" ";
+            ans.pop();
+            sizee--;
+        }
+        return output ;
     }
 };
