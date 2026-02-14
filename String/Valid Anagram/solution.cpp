80ea55c94547dd5fc096475d5023776088a7724b
Shreya Navada <shreyanavada26@gmail.com>
2026-02-14 21:54:51 +0530

    Create solution.cpp
    Given two strings s and t, return true if t is an anagram of s, and false otherwise.
    
     
    
    Example 1:
    
    Input: s = "anagram", t = "nagaram"
    
    Output: true
    
    Example 2:
    
    Input: s = "rat", t = "car"
    
    Output: false
    
     
    
    Constraints:
    
    1 <= s.length, t.length <= 5 * 104
    s and t consist of lowercase English letters.

---
+++ String/Valid Anagram/solution.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.size() != t.size())
+        {
+            return false;
+        }
+        vector<int>arr(26,0);
+        for(int i =0 ;i<s.size() ;i++)
+        {
+            arr[s[i] - 'a']++;
+            arr[t[i]-'a']--;
+        }
+        for(int i=0; i<26 ;i++)
+        {
+            if(arr[i] != 0)
+            {
+                return false;
+            }
+        }
+        return true;
+        
+    }
+};
