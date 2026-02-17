4983ad0e9226cf977154b18a7c3c5b79c32297ef
Shreya Navada <shreyanavada26@gmail.com>
2026-02-17 23:58:41 +0530

    Update solution.cpp
    Given a positive integer n, find the sum of all integers in the range [1, n] inclusive that are divisible by 3, 5, or 7.
    
    Return an integer denoting the sum of all numbers in the given range satisfying the constraint.
    
     
    
    Example 1:
    
    Input: n = 7
    Output: 21
    Explanation: Numbers in the range [1, 7] that are divisible by 3, 5, or 7 are 3, 5, 6, 7. The sum of these numbers is 21.
    Example 2:
    
    Input: n = 10
    Output: 40
    Explanation: Numbers in the range [1, 10] that are divisible by 3, 5, or 7 are 3, 5, 6, 7, 9, 10. The sum of these numbers is 40.
    Example 3:
    
    Input: n = 9
    Output: 30
    Explanation: Numbers in the range [1, 9] that are divisible by 3, 5, or 7 are 3, 5, 6, 7, 9. The sum of these numbers is 30.
     
    
    Constraints:
    
    1 <= n <= 103

--- Math/Sum Multiples/solution.cpp
+++ Math/Sum Multiples/solution.cpp
@@ -1,1 +1,23 @@
-ldkls
+class Solution {
+public:
+    int sumOfMultiples(int n) {
+        int sum =0 ;
+        for(int i=1 ;i<=n ;i++)
+        {
+            if(i%3==0)
+            {
+                sum += i;
+            }
+            else if(i%5==0)
+            {
+                sum += i;
+            }
+            else if(i%7 ==0 )
+            {
+                sum += i;
+            }
+        }
+        return sum;
+        
+    }
+};
