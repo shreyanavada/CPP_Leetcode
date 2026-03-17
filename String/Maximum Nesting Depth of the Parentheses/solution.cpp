18c97764224f40db90ce72c3294d4d564e27f911
Shreya Navada <shreyanavada26@gmail.com>
2026-03-18 02:39:41 +0530

    Create solution.cpp
    Given a valid parentheses string s, return the nesting depth of s. The nesting depth is the maximum number of nested parentheses.
    
     
    
    Example 1:
    
    Input: s = "(1+(2*3)+((8)/4))+1"
    
    Output: 3
    
    Explanation:
    
    Digit 8 is inside of 3 nested parentheses in the string.
    
    Example 2:
    
    Input: s = "(1)+((2))+(((3)))"
    
    Output: 3
    
    Explanation:
    
    Digit 3 is inside of 3 nested parentheses in the string.
    
    Example 3:
    
    Input: s = "()(())((()()))"
    
    Output: 3
    
     
    
    Constraints:
    
    1 <= s.length <= 100
    s consists of digits 0-9 and characters '+', '-', '*', '/', '(', and ')'.
    It is guaranteed that parentheses expression s is a VPS.

---
+++ String/Maximum Nesting Depth of the Parentheses/solution.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int maxDepth(string s) {
+        int open = 0;
+        int maxi = 0;
+        for(int i=0 ;i<s.size() ;i++)
+        {
+            if(s[i] == '(')
+            {
+                open++;
+                maxi = max(maxi , open);
+            }
+            else if(s[i] == ')')
+            {
+                open--;
+            }
+        }
+        return maxi;
+    }
+};
