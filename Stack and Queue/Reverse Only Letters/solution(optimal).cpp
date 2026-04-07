74932f7ece743f78741a6695ad5f94f98599942c
Shreya Navada <shreyanavada26@gmail.com>
2026-04-07 23:22:30 +0530

    Add optimal solution to reverse only letters in string
    Implement a solution to reverse only the letters in a string while maintaining the positions of non-letter characters. This approach uses O(1) space complexity.Given a string s, reverse the string according to the following rules:
    
    All the characters that are not English letters remain in the same position.
    All the English letters (lowercase or uppercase) should be reversed.
    Return s after reversing it.
    
     
    
    Example 1:
    
    Input: s = "ab-cd"
    Output: "dc-ba"
    Example 2:
    
    Input: s = "a-bC-dEf-ghIj"
    Output: "j-Ih-gfE-dCba"
    Example 3:
    
    Input: s = "Test1ng-Leet=code-Q!"
    Output: "Qedo1ct-eeLg=ntse-T!"
     
    
    Constraints:
    
    1 <= s.length <= 100
    s consists of characters with ASCII values in the range [33, 122].
    s does not contain '\"' or '\\'.

---
+++ Stack and Queue/Reverse Only Letters/solution(optimal).cpp
@@ -0,0 +1,30 @@
+// this is more optimal than using stack and has a SC of O(1)
+class Solution {
+public:
+    string reverseOnlyLetters(string s) {
+        int start =0 ;
+        int end = s.size()-1 ;
+        while(start<end)
+        {
+            if(isalpha(s[start]) && isalpha(s[end]))
+            {
+                swap(s[start],s[end]);
+                start++;
+                end--;
+            }
+            else
+            {
+                if(!(isalpha(s[start])))
+                {
+                    start++;
+                }
+                else
+                {
+                    end--;
+                }
+            }
+        }
+        return s ;
+        
+    }
+};
