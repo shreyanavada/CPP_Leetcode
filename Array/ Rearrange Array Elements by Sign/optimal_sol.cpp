7b4477d11ee2e77b2c17ec83575aaefdec0f9ef8
Shreya Navada <shreyanavada26@gmail.com>
2026-04-14 00:16:19 +0530

    Create optimal_sol.cpp
    You are given a 0-indexed integer array nums of even length consisting of an equal number of positive and negative integers.
    
    You should return the array of nums such that the array follows the given conditions:
    
    Every consecutive pair of integers have opposite signs.
    For all integers with the same sign, the order in which they were present in nums is preserved.
    The rearranged array begins with a positive integer.
    Return the modified array after rearranging the elements to satisfy the aforementioned conditions.
    
     
    
    Example 1:
    
    Input: nums = [3,1,-2,-5,2,-4]
    Output: [3,-2,1,-5,2,-4]
    Explanation:
    The positive integers in nums are [3,1,2]. The negative integers are [-2,-5,-4].
    The only possible way to rearrange them such that they satisfy all conditions is [3,-2,1,-5,2,-4].
    Other ways such as [1,-2,2,-5,3,-4], [3,1,2,-2,-5,-4], [-2,3,-5,1,-4,2] are incorrect because they do not satisfy one or more conditions.  
    Example 2:
    
    Input: nums = [-1,1]
    Output: [1,-1]
    Explanation:
    1 is the only positive integer and -1 the only negative integer in nums.
    So nums is rearranged to [1,-1].
     
    
    Constraints:
    
    2 <= nums.length <= 2 * 105
    nums.length is even
    1 <= |nums[i]| <= 105
    nums consists of equal number of positive and negative integers.

---
+++ Array/ Rearrange Array Elements by Sign/optimal_sol.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<int> rearrangeArray(vector<int>& nums) {
+        int n=nums.size();
+        vector<int>ans(n,0);
+        int posindex = 0 ;
+        int negindex = 1;
+        for(int i=0 ;i<nums.size() ;i++)
+        {
+            if(nums[i]>=0)
+            {
+                ans[posindex] = nums[i] ;
+                posindex = posindex+2;
+            }
+            else
+            {
+                ans[negindex] = nums[i] ;
+                negindex = negindex +2;
+            }
+        }
+        return ans;
+        
+    }
+};
+// space complexity : O(1)             time complexity : O(n) .
