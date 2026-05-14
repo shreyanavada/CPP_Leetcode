11f4139c9d5126b38484df658abc12aa602687f3
Shreya Navada <shreyanavada26@gmail.com>
2026-05-14 23:59:41 +0530

    Update solution.cpp

--- 2_Pointer/First Matching Character From Both Ends/solution.cpp
+++ 2_Pointer/First Matching Character From Both Ends/solution.cpp
@@ -1,1 +1,15 @@
-
+class Solution {
+public:
+    int firstMatchingIndex(string s) {
+        int n =s.size();
+        for(int i=0 ;i<s.size() ;i++)
+        {
+            if(s[i] == s[n-i-1])
+            {
+                return i;
+            }
+        }
+        return -1;
+        
+    }
+};
