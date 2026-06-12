08d5d0ee086e2d35cea976744d422e28c4cc6370
Shreya Navada <shreyanavada26@gmail.com>
2026-06-12 09:54:15 +0530

    Create optimal_solution.cpp

---
+++ String/Rotate String/optimal_solution.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool rotateString(string s, string goal) {
+        if(s.size() != goal.size())
+        {
+            return false;
+        }
+        s += s;
+        if (s.find(goal) != string::npos)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+};
+//TC O(n) . SC O(1) .
